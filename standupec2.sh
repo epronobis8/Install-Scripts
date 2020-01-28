c4f7fa8d13dbd950c954323245ee331c6a1f56b1
Epronobis8 <Epronobis8@gmail.com>
2020-01-28 12:27:31 -0500

    adding AWS script to test repo to merge later

---
+++ standupec2.sh
@@ -0,0 +1,2 @@
+aws ec2 run-instances --image-id ami-00068cd7555f543d5 --count 2 --instance-type t2.micro --key-name Newjenkins --security-group-ids sg-f7bee8a5 --subnet-id subnet-d607068a
+#this will stand up 2 ec2 linux instances
